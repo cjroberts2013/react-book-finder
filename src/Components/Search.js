1c9c92b49fc54f4f7ac5de0bfad4f5d076d6752f
CJ Roberts <cjroberts2013.gmail.com>
2020-02-15 16:45:48 -0600

    Finish the search component

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { Component } from "react";
 
-
+class Search extends Component {
+	render() {
+		return (
+			<div id="search-block">
+				<div
+					className="clear"
+					style={{
+						display: this.props.query.length > 0 ? "block" : "none"
+					}}
+					onClick={this.props.onClear}
+				>
+					<div className="clear__left"></div>
+					<div className="clear__right"></div>
+				</div>
+				<input
+					type="text"
+					value={this.props.query}
+					onChange={this.props.onChange}
+					onKeyPress={e => {
+						if (e.key === "Enter") {
+							this.props.onClick();
+						}
+					}}
+					placeholder="Search by author, title, etc..."
+				/>
+				<button id="search-btn" onClick={this.props.onClick}>
+					Search
+				</button>
+			</div>
+		);
+	}
+}
 
 export default Search;
